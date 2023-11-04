92f3570469ef6cf3e9b0aae342e9d28b49e21cf9
Myrna <myrnapt96@gmail.com>
2023-11-04 18:46:31 +0100

    FINAL

--- src/app/components/search-filters/search-filters.component.ts
+++ src/app/components/search-filters/search-filters.component.ts
@@ -7,6 +7,8 @@ import { Component, EventEmitter, Output } from '@angular/core';
 })
 export class SearchFiltersComponent {
 
+
+// ENVIAMOS LOS FILTROS AL PADRE
   @Output() statusFilterEvent = new EventEmitter<string>();
   @Output() genderFilterEvent = new EventEmitter<string>();
   @Output() speciesFilterEvent = new EventEmitter<string>();
@@ -25,11 +27,21 @@ export class SearchFiltersComponent {
     this.speciesFilterEvent.emit(species);
   }
 
+  // RESETEAMOS LOS FILTROS Y LOS BOTONES
   deselectAllFilters() {
     this.statusFilterEvent.emit('');
     this.genderFilterEvent.emit('');
     this.speciesFilterEvent.emit('');
+    this.deselectRadioButtons('input[name="options-status"]');
+    this.deselectRadioButtons('input[name="options-gender"]');
+    this.deselectRadioButtons('input[name="options"]');
   }
-  
+
+  deselectRadioButtons(selector: string) {
+    const radioButtons = document.querySelectorAll(selector) as NodeListOf<HTMLInputElement>;
+    radioButtons.forEach((radio: HTMLInputElement) => {
+      radio.checked = false;
+    });
+  }
 
 }
